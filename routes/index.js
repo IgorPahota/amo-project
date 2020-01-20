8636a8f12da77289c086a50cd2a123555221e2bd
Igor Pahota <eto.pochtovaya@gmail.com>
2020-01-20 16:37:37 +0300

    version with mailhook 5.0

--- routes/index.js
+++ routes/index.js
@@ -298,14 +298,13 @@ global.Headers = fetch.Headers;
 
 
 router.get('/mail/:id', async (req, res)=>{
-    // let hash = process.env.key;
-    // let login = process.env.login;
+    let hash = process.env.key;
+    let login = process.env.login;
     let id = req.params.id;
-    // console.log(hash);
     let myHeaders = new Headers();
     myHeaders.append("Accept", "*/*");
     myHeaders.append("Cache-Control", "no-cache");
-    myHeaders.append("Host", `prjctamoelbrus@yandex.com`);
+    myHeaders.append("Host", `${login}`);
     myHeaders.append("Accept-Encoding", "gzip, deflate, br");
     myHeaders.append("Content-Length", "");
     myHeaders.append("Connection", "keep-alive");
@@ -317,7 +316,7 @@ router.get('/mail/:id', async (req, res)=>{
     };
     let cookie;
 
-    await fetch(`https://secondelbrus.amocrm.ru/private/api/auth.php?USER_LOGIN=prjctamoelbrus@yandex.com&USER_HASH=935841b1d8108cd949645a6c754cd06840ed40eb&type=json`, requestOptions)
+    await fetch(`https://secondelbrus.amocrm.ru/private/api/auth.php?USER_LOGIN=${login}&USER_HASH=${hash}&type=json`, requestOptions)
         .then(response => {
             cookie = response.headers.raw()['set-cookie']
         }).catch(error => console.log(error));
@@ -333,7 +332,7 @@ router.get('/mail/:id', async (req, res)=>{
     myHeaders2.append("Content-Type", "application/json");
     myHeaders2.append("Accept", "*/*");
     myHeaders2.append("Cache-Control", "no-cache");
-    myHeaders2.append("Host", `prjctamoelbrus@yandex.com`);
+    myHeaders2.append("Host", `${login}`);
     myHeaders2.append("Accept-Encoding", "gzip, deflate, br");
     myHeaders2.append("Cookie", `user_lang=ru; ${cookieForPost[0]}`);
     myHeaders2.append("Connection", "keep-alive");
